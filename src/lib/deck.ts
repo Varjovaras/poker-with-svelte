84871c270af2f3bc93b5d5792879cd2eaaa86970
Kristjan Rajaniemi <kristjan.rajaniemi@live.fi>
2024-02-28 22:48:51 +0200

    shuffle algorithm

--- src/lib/deck.ts
+++ src/lib/deck.ts
@@ -1,19 +1,34 @@
-const suits = ['hearts', 'diamonds', 'spades', 'clubs'] as const;
-const ranks = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12] as const;
+export type Card = { suit: string; rank: number };
 
-type Card = { suit: string; rank: number };
-
-class Deck {
+export class Deck {
+	static readonly suits = ['hearts', 'diamonds', 'spades', 'clubs'];
+	static readonly ranks = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 	deck: Card[] = [];
+
 	constructor() {
 		for (let i = 0; i < 4; i++) {
 			for (let j = 0; j < 13; j++) {
 				const card = {
-					suit: suits[i],
-					rank: ranks[j]
+					suit: Deck.suits[i],
+					rank: Deck.ranks[j]
 				};
 				this.deck.push(card);
 			}
 		}
+		if (this.deck.length !== 52) {
+			throw new Error('deck is not 52 cards');
+		}
+		this.shuffle();
+	}
+
+	//fisher-yates shuffle algorithm
+	shuffle() {
+		const array = this.deck;
+		let i = array.length;
+		while (--i > 0) {
+			const temp = Math.floor(Math.random() * (i + 1));
+			[array[temp], array[i]] = [array[i], array[temp]];
+		}
+		this.deck = array;
 	}
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
-	const suits = ['hearts', 'diamonds', 'spades', 'clubs'] as const;
-	const ranks = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12] as const;
+	import { Deck } from '$lib/deck';
+	let deck = new Deck().deck;
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+{#each deck as card}
+	<p>{card.suit} . {card.rank}</p>
+{/each}
