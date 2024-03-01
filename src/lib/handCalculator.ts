c83d4228d55479b697b94c752df57ba4c7b7e51d
Kristjan Rajaniemi <kristjan.rajaniemi@live.fi>
2024-03-01 14:16:07 +0200

    is flush implementation

--- src/lib/handCalculator.ts
+++ src/lib/handCalculator.ts
@@ -17,6 +17,10 @@ type HandValue = (typeof HandTypes)[number];
 type FlushAndStraightHandTypes = 'RoyalFlush' | 'StraightFlush' | 'Flush';
 
 export const handCalculator = (hand: Card[]): HandValue => {
+	if (hand.length < 5) {
+		throw new Error('Hand size smaller than 5 is not possible to calculate');
+	}
+
 	if (isFlush(hand) && isStraight(hand)) {
 		return isRoyalFlush(hand);
 	}
@@ -27,10 +31,10 @@ export const handCalculator = (hand: Card[]): HandValue => {
 //this gets copied for isStraight() function
 const straightHelper = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-//ace which is always rank of 0 is also added to the 14th element of the array
 const isStraight = (hand: Card[]): boolean => {
 	const handRanksAsArray = [...straightHelper];
 	for (let i = 0; i < hand.length; i++) {
+		//ace which has rank of 0 is also added to the 14th element of the array
 		if (hand[i].rank === 0) {
 			handRanksAsArray[0]++;
 			handRanksAsArray[13]++;
@@ -39,8 +43,8 @@ const isStraight = (hand: Card[]): boolean => {
 		}
 	}
 
+	//counter used to track how many elements hand has in row
 	let counter = 0;
-
 	//array ends at 10 because if handRanksAsArray[9] is not part of the straight
 	//it cannot be straight
 	for (let i = 0; i < 10; i++) {
@@ -57,7 +61,24 @@ const isStraight = (hand: Card[]): boolean => {
 };
 
 const isFlush = (hand: Card[]): boolean => {
-	return true;
+	const flushHelper = [0, 0, 0, 0];
+	for (let i = 0; i < hand.length; i++) {
+		switch (hand[i].suit) {
+			case 'hearts':
+				flushHelper[0]++;
+				break;
+			case 'diamonds':
+				flushHelper[1]++;
+				break;
+			case 'clubs':
+				flushHelper[2]++;
+				break;
+			case 'spades':
+				flushHelper[3]++;
+				break;
+		}
+	}
+	return flushHelper.some((count) => count >= 5);
 };
 
 //this is only called when your hand has flush and straight at the same time.
