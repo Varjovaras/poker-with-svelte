8fe13c86397fb93d2794a2589c7f1f453c5aced2
Kristjan Rajaniemi <kristjan.rajaniemi@live.fi>
2024-07-18 21:28:16 +0300

    fix naming of NewHand component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,40 +1,40 @@
 <script lang="ts">
-	import type { PageData } from './$types';
-	import Table from '$lib/components/Table.svelte';
-	// import ShuffleButton from '$lib/components/ShuffleButton.svelte';
-	import { newDeck } from '$lib/poker/deck';
-	import { deal, type Poker } from '$lib/poker/poker';
-	import DealButton from '$lib/components/DealButton.svelte';
-	import NewHand from '$lib/components/newHand.svelte';
+import type { PageData } from "./$types";
+import Table from "$lib/components/Table.svelte";
+// import ShuffleButton from '$lib/components/ShuffleButton.svelte';
+import { newDeck } from "$lib/poker/deck";
+import { deal, type Poker } from "$lib/poker/poker";
+import DealButton from "$lib/components/DealButton.svelte";
+import NewHand from "$lib/components/NewHand.svelte";
 
-	export let data: PageData;
-	const poker: Poker = data.post.poker;
+export let data: PageData;
+const poker: Poker = data.post.poker;
 
-	let numberOfCardsOnTable = 0;
-	$: cardsOnTable = poker.deck.slice(0, numberOfCardsOnTable);
+let numberOfCardsOnTable = 0;
+$: cardsOnTable = poker.deck.slice(0, numberOfCardsOnTable);
 
-	// const shuffle = () => {
-	// 	poker.deck = newDeck();
-	// 	for (const player of poker.players) {
-	// 		player.cards = [];
-	// 	}
-	// 	deal(poker);
-	// };
+// const shuffle = () => {
+// 	poker.deck = newDeck();
+// 	for (const player of poker.players) {
+// 		player.cards = [];
+// 	}
+// 	deal(poker);
+// };
 
-	const dealOnTable = () => {
-		if (numberOfCardsOnTable === 0) {
-			numberOfCardsOnTable = 3;
-		} else numberOfCardsOnTable += 1;
-	};
+const dealOnTable = () => {
+	if (numberOfCardsOnTable === 0) {
+		numberOfCardsOnTable = 3;
+	} else numberOfCardsOnTable += 1;
+};
 
-	const newHand = () => {
-		numberOfCardsOnTable = 0;
-		poker.deck = newDeck();
-		for (const player of poker.players) {
-			player.cards = [];
-		}
-		deal(poker);
-	};
+const newHand = () => {
+	numberOfCardsOnTable = 0;
+	poker.deck = newDeck();
+	for (const player of poker.players) {
+		player.cards = [];
+	}
+	deal(poker);
+};
 </script>
 
 <div class="flex min-h-screen flex-col items-center justify-center">
